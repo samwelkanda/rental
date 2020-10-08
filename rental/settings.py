cbf89c80b4875defe07e226db4375ee14f0e968c
Sam Kiptoo <samwelkanda@gmail.com>
2020-10-08 07:36:29 +0300

    Initalize the api django app

--- rental/settings.py
+++ rental/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rental.apps.api',
+
+
+    'djangorestframework',
 ]
 
 MIDDLEWARE = [
